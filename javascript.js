d05e33ce0bd86f74cf62255c0296e8dab04f2af5
Raul Villar <raultheviking@gmail.com>
2022-12-20 09:40:18 +0100

    Added Background music

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btn");
+
+btn.onclick = () => {
+  const audio = new Audio(
+    "C:\Users\rault\Desktop\Programación\Proyectos\SH Wheel of DOOM\Pictures\risaIbai.mp3"
+  );
+
+  audio.play();
+};
