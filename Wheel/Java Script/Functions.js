84dee370406666430b2b9d6d02e4bb3efdd485c4
celiacueria <celiacueria1999@gmail.com>
2022-12-21 11:59:14 +0100

    listo

--- Wheel/Java Script/Functions.js
+++ Wheel/Java Script/Functions.js
@@ -18,20 +18,42 @@ function myfunction(){
 function updateText() {
     // Obtén el valor del input
     var inputText = document.getElementById("input-1").value;
-    var inputText = document.getElementById("input-2").value;
-    var inputText = document.getElementById("input-3").value;
-    var inputText = document.getElementById("input-4").value;
-    var inputText = document.getElementById("input-5").value;
-    var inputText = document.getElementById("input-6").value;
 
     // Establece el valor del input como el contenido del elemento text-container
     document.getElementById("coder-1").innerHTML = inputText;
-    document.getElementById("coder-2").innerHTML = inputText;
-    document.getElementById("coder-3").innerHTML = inputText;
-    document.getElementById("coder-4").innerHTML = inputText;
-    document.getElementById("coder-5").innerHTML = inputText;
-    document.getElementById("coder-6").innerHTML = inputText;
 
+}
 
+function updateText2(){
+    var inputText = document.getElementById("input-2").value;
+    document.getElementById("coder-2").innerHTML =inputText;
+}
+ 
+function updateText3(){
+    var inputText = document.getElementById("input-3").value;
+    document.getElementById("coder-3").innerHTML =inputText;
+}
+function updateText4(){
+    var inputText = document.getElementById("input-4").value;
+    document.getElementById("coder-4").innerHTML =inputText;
+}
+function updateText5(){
+    var inputText = document.getElementById("input-5").value;
+    document.getElementById("coder-5").innerHTML =inputText;
+}
+ 
+function updateText6(){
+    var inputText = document.getElementById("input-6").value;
+    document.getElementById("coder-6").innerHTML =inputText;
+}
+ 
+function updateText7(){
+    var inputText = document.getElementById("input-7").value;
+    document.getElementById("coder-7").innerHTML =inputText;
+}
+ 
+function updateText8(){
+    var inputText = document.getElementById("input-8").value;
+    document.getElementById("coder-8").innerHTML =inputText;
 }
  
